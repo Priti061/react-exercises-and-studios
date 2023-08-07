a52f38748532dddd9ee9e302f5f811ebbf55f704
Priti061 <srijanabaral94@gmail.com>
2023-08-06 22:59:26 -0500

    changed banner, button and profile file

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,14 +1,29 @@
 import './styles.css';
 import Button from './Button';
-
-
+import oceans from './oceans.json';
+const listItem= oceans.map(ocean =>
+   <div className= {`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`} key= {ocean.id}>
+   <img src= {ocean.image}
+   alt= {ocean.name}
+   className="img"/>
+   <h1>{ocean.name}</h1>
+   <h3>Fun Facts:</h3>
+   <ul>
+      <li>{ocean.fact1}</li>
+      <li>{ocean.fact2}</li>
+      <li>{ocean.fact3}</li>
+   </ul>
+   <Button />
+   </div>
+   
+   );
 function Profile()
 {
    return(
-      <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
-      </>  
+      <ul> 
+         {listItem}
+      </ul>
+         
    );
 }
 
