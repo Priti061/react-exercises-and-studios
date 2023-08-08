dc00ae8cde461d9c75cc2079c81828e81a5213d9
Priti061 <srijanabaral94@gmail.com>
2023-08-08 16:29:14 -0500

    modified button file and added code to RateARecipe

--- part2/studio/src/components/RateARecipe.js
+++ part2/studio/src/components/RateARecipe.js
@@ -1,7 +1,12 @@
 let stars = ["⭐", "⭐ ⭐", "⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐ ⭐"];
 
-function RateARecipe() {
-  return stars;
+function RateARecipe(props) {
+  function GiveRating(){
+    
+    return <h3>{stars[props.rating - 1]}</h3>;
+}
+const isValidNum = props.rating <= 5 && props.rating >=1;
+ return isValidNum ? <GiveRating /> : null;
 }
 
 export default RateARecipe;
